aebad15e5f6bd52dd9163d32e318b3420960480c
Anas Al-Mughamsi <Anas.Mughamsi@gmail.com>
2021-09-16 23:15:24 +0300

    noithing

---
+++ src/Calculation.java
@@ -0,0 +1,27 @@
+public class Calculation {
+
+    public int addition(int x, int y) {
+        return x + y;
+    }
+
+    public int subtraction(int x, int y) {
+        return x - y;
+    }
+
+    public int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public int division(int x, int y) {
+        return x / y;
+    }
+
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation();
+
+        System.out.println(calculation.addition(2,5));
+        System.out.println(calculation.subtraction(2,5));
+        System.out.println(calculation.multiplication(2,5));
+        System.out.println(calculation.division(4,2));
+    }
+}
